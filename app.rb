e0870884c959a4a8b71a55c794eb70bf716b8e38
Henrik Dittmar <henrik.dittmar@gmx.de>
2018-01-22 13:40:58 +0100

    Boilerplate sinatra app

---
+++ app.rb
@@ -0,0 +1,20 @@
+require "sinatra"
+require "sinatra/reloader" if development?
+require "pry-byebug"
+require "better_errors"
+configure :development  do
+  use BetterErrors::Middleware
+  BetterErrors.application_root = File.expand_path("..", __FILE__)
+end
+
+get '/hello' do
+  'Hello world'
+end
+
+get '/' do
+  '/test or /hello'
+end
+
+get '/test' do
+  'test'
+end
